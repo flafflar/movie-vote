95af44fc570119aa002eee8afd0b635fe3a8b8ee
flafflar <achmichail@gmail.com>
2023-04-13 14:34:56 +0300

    Create a button that is used to OAuth with Instagram

---
+++ frontend/src/components/InstagramLoginButton.jsx
@@ -0,0 +1,15 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons';
+
+import './InstagramLoginButton.css';
+
+export default function InstagramLoginButton() {
+	const clientId = 766242878400532;
+	const redirectUri = `${window.location.origin}/oauth`;
+	const oAuthUri = `https://api.instagram.com/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=code&scope=user_profile`;
+
+	return <div className='button' onClick={() => window.open(oAuthUri, '_self', 'popup=true,noreferer')}>
+		<FontAwesomeIcon icon={faInstagram} size='xl' className='icon' />
+		Login with Instagram
+	</div>
+}
