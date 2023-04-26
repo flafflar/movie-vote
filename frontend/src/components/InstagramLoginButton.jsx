8bd9e96ae967363d5537975d8a1f2c7530a2a78b
flafflar <achmichail@gmail.com>
2023-04-26 14:43:26 +0300

    Made the OAuth redirect URL relative to the current path

--- frontend/src/components/InstagramLoginButton.jsx
+++ frontend/src/components/InstagramLoginButton.jsx
@@ -5,7 +5,7 @@ import './InstagramLoginButton.css';
 
 export default function InstagramLoginButton() {
 	const clientId = 766242878400532;
-	const redirectUri = `${window.location.origin}/oauth`;
+	const redirectUri = `${window.location.origin}${window.location.pathname}/oauth`;
 	const oAuthUri = `https://api.instagram.com/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=code&scope=user_profile`;
 
 	return <div className='button' onClick={() => window.open(oAuthUri, '_self', 'popup=true,noreferer')}>
