aa58920b381e7806cbc14e60ca159d8a51c372ed
flafflar <achmichail@gmail.com>
2023-04-26 12:16:50 +0300

    Create endpoints that allow users to cast votes

--- src/routes/api.js
+++ src/routes/api.js
@@ -51,4 +51,66 @@ router.get('/votes', checkAuthTokenMiddleware, (req, res) => {
     })
 })
 
+/* Returns the users who voted for a specific movie */
+router.get('/votes/movie/:movieId', checkAuthTokenMiddleware, (req, res) => {
+    db.query(`SELECT users.id, users.username FROM votes
+        INNER JOIN users ON votes.user_id = users.id
+        WHERE votes.movie_id = ?`,
+    [req.params.movieId], (err, results, fields) => {
+        if (err) {
+            res.status(500);
+            res.json({error: err});
+            return;
+        }
+
+        res.json(results);
+    })
+})
+
+/* Casts a vote to the specific movie */
+router.post('/votes/movie/:movieId', checkAuthTokenMiddleware, (req, res) => {
+    // Check if the user has already voted
+    db.query(`SELECT COUNT(user_id) AS count FROM votes WHERE user_id = ?`,
+    [req.userId], (err, results, fields) => {
+        if (err) {
+            res.status(500);
+            res.json({error: err});
+            return;
+        }
+
+        if (results[0].count > 0){
+            res.status(409);
+            res.end();
+            return;
+        }
+
+        db.query(`INSERT INTO votes(movie_id, user_id) VALUES (?, ?)`,
+        [req.params.movieId, req.userId], (err, results, fields) => {
+            if (err) {
+                res.status(500);
+                res.json({error: err});
+                return;
+            }
+
+            res.end();
+        })
+    })
+})
+
+/* Returns the movies voted by the authenticated user. Useful for checking if
+ * the user has already voted or not. */
+router.get('/votes/user', checkAuthTokenMiddleware, (req, res) => {
+    db.query(`SELECT movie_id FROM votes WHERE user_id = ?`,
+    [req.userId], (err, results, fields) => {
+        if (err) {
+            res.status(500);
+            res.json({error: err});
+            return;
+        }
+
+        const data = results.map(row => row.movie_id);
+        res.json(data);
+    })
+})
+
 module.exports = router;
