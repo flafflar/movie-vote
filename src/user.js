a295f0f98269ec10e88235483474e48d417d3d41
flafflar <achmichail@gmail.com>
2023-04-29 21:30:04 +0300

    Check if the user is valid before trying to return their ID
    Fixes #3

--- src/user.js
+++ src/user.js
@@ -31,7 +31,12 @@ function checkUserAuthToken(token, callback){
 	db.query(`SELECT user_id FROM user_tokens WHERE token = ?`, [token], (err, results, fields) => {
 		if (err) return callback(err);
 
-		callback(null, results.length !== 0, results[0].user_id);
+		if (results.length === 0){
+			callback(null, false, null);
+		} else {
+			callback(null, true, results[0].user_id);
+		}
+
 	})
 }
 
