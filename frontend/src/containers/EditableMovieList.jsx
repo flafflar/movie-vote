9b6a25a5a101f55e5c7b45daccecf96d339373ec
flafflar <achmichail@gmail.com>
2023-04-29 13:58:06 +0300

    Make the edit button of each movie display the modal

--- frontend/src/containers/EditableMovieList.jsx
+++ frontend/src/containers/EditableMovieList.jsx
@@ -1,15 +1,20 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPenToSquare, faPlusSquare } from '@fortawesome/free-regular-svg-icons';
 
 import { MoviesContext } from '../Main';
 import MovieRow from '../components/MovieRow';
 import IconButton from '../components/IconButton';
+import ModalWindow from '../components/ModalWindow';
+import MovieEditor from './MovieEditor';
 
 import './MovieList.css';
 
 export default function EditableMovieList() {
 	const {movies} = useContext(MoviesContext);
+
+	const [showModal, setShowModal] = useState(false);
+	const [modalMovieId, setModalMovieId] = useState(null);
 
 	const movieRows = movies?.map(movie => (
 		<MovieRow
@@ -18,7 +23,10 @@ export default function EditableMovieList() {
 			posterUrl={movie.posterImageUrl}
 			totalVotes={0}
 			actionElement={
-				<div className='action-button'>
+				<div className='action-button' onClick={() => {
+					setModalMovieId(movie.id);
+					setShowModal(true);
+				}}>
 					<FontAwesomeIcon icon={faPenToSquare} size='xl' />
 				</div>
 			}
@@ -33,5 +41,12 @@ export default function EditableMovieList() {
 			icon={<FontAwesomeIcon icon={faPlusSquare} size='xl' />}
 			text="Add new"
 		/>
+		<ModalWindow
+			title="Edit movie"
+			show={showModal}
+			onHide={() => setShowModal(false)}
+		>
+			<MovieEditor forMovieId={modalMovieId} />
+		</ModalWindow>
 	</>
 }
--- frontend/src/containers/MovieEditor.jsx
+++ frontend/src/containers/MovieEditor.jsx
@@ -1,13 +1,37 @@
+import { useContext, useEffect, useState } from 'react';
+import { MoviesContext } from '../Main';
+
 import './MovieEditor.css';
 
-export default function MovieEditor() {
+export default function MovieEditor({ forMovieId }) {
+	const {movies} = useContext(MoviesContext);
+
+	const [title, setTitle] = useState('');
+	const [posterUrl, setPosterUrl] = useState('');
+
+	useEffect(() => {
+		if (forMovieId !== null){
+			const movie = movies.find(movie => movie.id === forMovieId);
+			setTitle(movie.title);
+			setPosterUrl(movie.posterImageUrl);
+		}
+	}, [movies, forMovieId]);
+
 	return (
 		<form className="movie-editor">
 			<div className="inputs">
 				<label for="movie-title-input">Title:</label>
-				<input id="movie-title-input" type='text' />
+				<input
+					id="movie-title-input"
+					type='text'
+					value={title}
+				/>
 				<label for="movie-poster-url-input">Poster image URL:</label>
-				<input id="movie-poster-url-input" type='url' />
+				<input
+					id="movie-poster-url-input"
+					type='url'
+					value={posterUrl}
+				/>
 			</div>
 			<div className="buttons">
 				<div className="button">Cancel</div>
